c372b621ae6438aa432880f922fa08af5e6ed2b9
sezeef <seezef.h@gmail.com>
2022-12-27 09:20:57 +0200

    extended tailwind with grays for /app

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,10 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-gray-100": "#b9bbbe",
+        "dark-gray-300": "#36393f",
+        "dark-gray-500": "#2f3136",
+        "dark-gray-700": "#292b2f",
+        "dark-gray-900": "202225",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  // daisyui: {
-  //   themes: ["dracula"],
-  // },
 };
